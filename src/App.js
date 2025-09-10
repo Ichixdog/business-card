f7b2644b58b467c91adb1eef62d6d324ef98dd59
Dmitriy <ichixdog@gmail.com>
2025-09-10 14:34:05 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import './assets/styles/global.scss'
+import Header from './components/Header/Header'
+import Main from './components/Main/Main'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
